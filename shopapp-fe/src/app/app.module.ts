9ca97028035a10484c4277c51a6535415a66c090
huyhuynh1905 <huyhuynh1905@gmail.com>
2024-05-28 00:44:17 +0700

    [U] section 1 - lesson 8: dev page detail product component

--- shopapp-fe/src/app/app.module.ts
+++ shopapp-fe/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { OrderComponent } from './order/order.component';
 import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
 
 
 
@@ -19,7 +20,8 @@ import { RegisterComponent } from './register/register.component';
     OrderComponent,
     OrderConfirmComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    DetailProductComponent
   ],
   imports: [
     BrowserModule
@@ -29,7 +31,8 @@ import { RegisterComponent } from './register/register.component';
     // OrderComponent,
     // OrderConfirmComponent,
     // LoginComponent,
-    RegisterComponent
+    // RegisterComponent,
+    DetailProductComponent
   ]
 })
 export class AppModule { }
